c62ade27ae85a1751944d54f2f3f35fc8232e482
John <johnathanlee72@hotmail.com>
2022-04-12 08:37:51 -0700

    Added TabView to include featured and list tabs

--- Recipe List App/Views/ContentView.swift
+++ Recipe List App/Views/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
                         }
                     }
                 )
+                .navigationBarTitle("All Recipes")
                 
             }
         }
---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Johnathan Lee on 4/12/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            ContentView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
