481dbfe8dd70a8dc291aa5119961b81afa341160
John <johnathanlee72@hotmail.com>
2022-04-18 15:54:51 -0700

    - Change list to scrollview - Update format on list view

--- Recipe List App/Views/ContentView.swift
+++ Recipe List App/Views/ContentView.swift
@@ -14,33 +14,51 @@ struct ContentView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        NavigationView {
-            List(model.recipes) {
-                r in
+        
+            NavigationView {
                 
-                NavigationLink(
-                    destination: RecipeDetailView(recipe:r),
-                    label: {
-                        HStack {
-                            Image(r.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50, alignment: .center)
-                                .clipped()
-                            Text(r.name)
-                        }
+                VStack (alignment: .leading) {
+                    
+                    Text("All Recipes")
+                        .bold()
+                        .padding(.leading)
+                        .font(.largeTitle)
+                
+                    ScrollView {
+                        LazyVStack (alignment: .leading) {
+                            ForEach(model.recipes) {
+                                r in
+                                
+                                NavigationLink(
+                                    destination: RecipeDetailView(recipe:r),
+                                    label: {
+                                        HStack {
+                                            Image(r.image)
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: 50, height: 50, alignment: .center)
+                                                .clipped()
+                                                .cornerRadius(5)
+                                            Text(r.name)
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                )
+                            }
+                        }.padding(.leading)
                     }
-                )
-                .navigationBarTitle("All Recipes")
-                
+                }
+//                .navigationBarTitle("All Recipes")
+                .navigationBarHidden(true)
             }
-        }
-        .navigationBarTitle("All Recipes")
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(RecipeModel())
+            .previewInterfaceOrientation(.portrait)
     }
 }
